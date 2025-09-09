8d41a175b578d4b07ed9d5e0d11d4fb985e3a5c9
Abolfzl80 <abolpoot8@gmail.com>
2025-09-09 15:45:54 +0330

    frist push

---
+++ README.md
@@ -0,0 +1,21 @@
+
+## About This Project
+
+this is simple and usefull Authentication project With JWT!
+
+## Start:
+
+git clone https://github.com/Abolfzl80/Laravel_Auth_Api_Project.git
+composer install
+php artisan migrate
+php artisan serve
+
+## Routes
+
+POST       http://127.0.0.1:8000/api/register 
+POST       http://127.0.0.1:8000/api/login 
+POST       http://127.0.0.1:8000/api/logout  
+
+# Creator
+
+GitHub: @Abolfzl80
---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        try{
+            $request->validated();
+            $user = User::create([
+                        'name' => $request->name,
+                        'email' => $request->email,
+                        'password' => Hash::make($request->password), 
+                    ]);
+            $token = JWTAuth::fromUser($user);
+            return response()->json([
+                'message' => 'Registred Successfully!',
+                'token' => $token,
+            ]);
+        }catch(\Exception $e){
+            return response()->json(['Error' => "$e"]);
+        }
+
+    }
+
+    public function login(LoginRequest $request)
+    {
+        try{
+            $request->validated();
+            $user = User::where('email', $request->email)->first();
+            if(!$user || !Hash::check($request->password, $user->password)){
+                return response()->json(['Error' => 'Invalid Email or Password']);
+            }
+            $token = JWTAuth::fromUser($user);
+            return response()->json([
+                'token' => $token,
+            ]);
+        }catch(\Exception $e){
+            return response()->json(['Error' => "$e"]);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        try{
+            $user = $request->user();
+            JWTAuth::invalidate(JWTAuth::getToken($user));
+            return response()->json(['message' => 'Logout Successfully!']);
+        }catch(\Exception $e){
+            return response()->json(['Error' => "$e"]);
+        }
+    }
+}
